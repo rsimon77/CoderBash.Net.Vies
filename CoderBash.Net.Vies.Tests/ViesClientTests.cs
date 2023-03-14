cc98d0a41de5ab29daf70aacad73edccce56eac8
Robert Simon <rsimon@hotmail.hu>
2023-03-14 22:17:36 +0100

    Added HttpClient Timeout and Batch validation

--- CoderBash.Net.Vies.Tests/ViesClientTests.cs
+++ CoderBash.Net.Vies.Tests/ViesClientTests.cs
@@ -1,94 +1,133 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 using CoderBash.Net.Vies.Clients;
 using CoderBash.Net.Vies.Enums;
 using CoderBash.Net.Vies.Exceptions;
 
 namespace CoderBash.Net.Vies.Tests
 {
-	[TestFixture(
-		Author = "NicolasDemarbaix",
-		Category = "CoderBash.Net.Vies",
-		Description = "Vies Client tests",
-		TestOf = typeof(ViesClient))]
-	public class ViesClientTests
-	{
-		private const string TEST_VAT_NUMBER = "0729739314";
-		private const string TEST_INVALID = "0000000000";
-		private const string TEST_INVALID_FORMAT = "0729-739-314";
-		private const string TEST_COMPANY_NAME = "DEMARBIT";
+    [TestFixture(
+        Author = "NicolasDemarbaix",
+        Category = "CoderBash.Net.Vies",
+        Description = "Vies Client tests",
+        TestOf = typeof(ViesClient))]
+    public class ViesClientTests
+    {
+        private const string TEST_VAT_NUMBER = "0729739314";
+        private const string TEST_INVALID = "0000000000";
+        private const string TEST_INVALID_FORMAT = "0729-739-314";
+        private const string TEST_COMPANY_NAME = "DEMARBIT";
 
-		[Test(
-			Author = "NicolasDemarbaix",
-			Description = "Fetch valid VAT information")]
-		public void Test_Vies_FetchValidInfo()
-		{
-			using var client = new ViesClient();
+        [Test(
+            Author = "NicolasDemarbaix",
+            Description = "Fetch valid VAT information")]
+        public void Test_Vies_FetchValidInfo()
+        {
+            using var client = new ViesClient();
 
-			Assert.DoesNotThrowAsync(async () =>
-			{
-				var vatResult = await client.ValidateVatNumberAsync(Enums.EUCountryCodes.BE, TEST_VAT_NUMBER);
+            Assert.DoesNotThrowAsync(async () =>
+            {
+                var vatResult = await client.ValidateVatNumberAsync(Enums.EUCountryCodes.BE, TEST_VAT_NUMBER);
 
-				Assert.Multiple(() =>
-				{
-					Assert.That(vatResult, Is.Not.Null);
-					Assert.That(vatResult.IsValid, Is.True);
-					Assert.That(vatResult.Name, Is.EqualTo(TEST_COMPANY_NAME));
-				});
-			});
-		}
+                Assert.Multiple(() =>
+                {
+                    Assert.That(vatResult, Is.Not.Null);
+                    Assert.That(vatResult.IsValid, Is.True);
+                    Assert.That(vatResult.Name, Is.EqualTo(TEST_COMPANY_NAME));
+                });
+            });
+        }
 
-		[Test(
-			Author = "NicolasDemarbaix",
-			Description = "Fetch valid VAT information (using country code)")]
-		public void Test_Vies_FetchValidInfoUsingStringCode()
-		{
-			using var client = new ViesClient();
+        [Test(
+            Author = "NicolasDemarbaix",
+            Description = "Fetch valid VAT information (using country code)")]
+        public void Test_Vies_FetchValidInfoUsingStringCode()
+        {
+            using var client = new ViesClient();
 
-			Assert.DoesNotThrowAsync(async () =>
-			{
-				var vatResult = await client.ValidateVatNumberAsync("BE", TEST_VAT_NUMBER);
+            Assert.DoesNotThrowAsync(async () =>
+            {
+                var vatResult = await client.ValidateVatNumberAsync("BE", TEST_VAT_NUMBER);
 
-				Assert.Multiple(() =>
-				{
-					Assert.That(vatResult, Is.Not.Null);
-					Assert.That(vatResult.IsValid, Is.True);
-					Assert.That(vatResult.Name, Is.EqualTo(TEST_COMPANY_NAME));
-					Assert.That(vatResult.Country, Is.EqualTo(EUCountryCodes.BE));
-				});
-			});
-		}
+                Assert.Multiple(() =>
+                {
+                    Assert.That(vatResult, Is.Not.Null);
+                    Assert.That(vatResult.IsValid, Is.True);
+                    Assert.That(vatResult.Name, Is.EqualTo(TEST_COMPANY_NAME));
+                    Assert.That(vatResult.Country, Is.EqualTo(EUCountryCodes.BE));
+                });
+            });
+        }
 
-		[Test(
-			Author = "NicolasDemarbaix",
-			Description = "Fetch invalid VAT information")]
-		public void Test_Vies_FetchInvalidInfo()
-		{
-			using var client = new ViesClient();
+        [Test(
+            Author = "NicolasDemarbaix",
+            Description = "Fetch invalid VAT information")]
+        public void Test_Vies_FetchInvalidInfo()
+        {
+            using var client = new ViesClient();
 
-			Assert.DoesNotThrowAsync(async () =>
-			{
-				var vatResult = await client.ValidateVatNumberAsync(EUCountryCodes.BE, TEST_INVALID);
+            Assert.DoesNotThrowAsync(async () =>
+            {
+                var vatResult = await client.ValidateVatNumberAsync(EUCountryCodes.BE, TEST_INVALID);
 
-				Assert.Multiple(() =>
-				{
-					Assert.That(vatResult, Is.Not.Null);
-					Assert.That(vatResult.IsValid, Is.False);
-				});
-			});
-		}
+                Assert.Multiple(() =>
+                {
+                    Assert.That(vatResult, Is.Not.Null);
+                    Assert.That(vatResult.IsValid, Is.False);
+                });
+            });
+        }
 
-		[Test(
-			Author = "NicolasDemarbaix",
-			Description = "Incorrect VAT number format")]
-		public void Test_Vies_InvalidVatNumberFormat()
-		{
-			using var client = new ViesClient();
+        [Test(
+            Author = "NicolasDemarbaix",
+            Description = "Incorrect VAT number format")]
+        public void Test_Vies_InvalidVatNumberFormat()
+        {
+            using var client = new ViesClient();
 
-			Assert.ThrowsAsync<ViesRequestException>(async () =>
-			{
-				await client.ValidateVatNumberAsync(EUCountryCodes.BE, TEST_INVALID_FORMAT);
-			});
-		}
-	}
+            Assert.ThrowsAsync<ViesRequestException>(async () =>
+            {
+                await client.ValidateVatNumberAsync(EUCountryCodes.BE, TEST_INVALID_FORMAT);
+            });
+        }
+
+        [Test(
+            Author = "Robert Simon",
+            Description = "HttpClient Timeout configure")]
+        public void Test_Vies_TestHttpClientTimeout()
+        {
+            using var client = new ViesClient(new TimeSpan(0, 3, 0));
+            Assert.DoesNotThrowAsync(async () =>
+            {
+                var vatResult = await client.ValidateVatNumberAsync(EUCountryCodes.BE, TEST_VAT_NUMBER);
+
+                Assert.Multiple(() =>
+                {
+                    Assert.That(vatResult, Is.Not.Null);
+                    Assert.That(vatResult.IsValid, Is.True);
+                });
+            });
+        }
+
+
+        [Test(Author = "Robert Simon", Description = "Check multiple vat number validation")]
+        public void Test_Validate_Multiple_VATNumber()
+        {
+            var vatNumbers = new List<string>() { "DE327498938", "DE299733535", "DE260341043", "FR09238631849", "BE0364430967" };
+            using var client = new ViesClient();
+            Assert.DoesNotThrowAsync(async () =>
+            {
+                var validationResult = await client.ValidateMultipleVatNumbersAsync(vatNumbers);
+
+                Assert.Multiple(() =>
+                {
+                    Assert.That(validationResult, Is.Not.Null);
+                    Assert.That(validationResult, Is.All.Exist);
+                    Assert.That(validationResult, Is.All.False);
+                });
+            });
+
+        }
+    }
 }
 
--- CoderBash.Net.Vies/Clients/ViesClient.cs
+++ CoderBash.Net.Vies/Clients/ViesClient.cs
@@ -7,20 +7,19 @@ using Newtonsoft.Json;
 
 namespace CoderBash.Net.Vies.Clients
 {
-	/// <summary>
-	/// Client for requesting a VAT number validation from the VIES service.
-	/// </summary>
-	public sealed class ViesClient : IDisposable
-	{
-		private readonly HttpClient _client;
-
-		/// <summary>
-		/// 
-		/// </summary>
-		public ViesClient()
-		{
-			_client = SetupClient();
-		}
+    /// <summary>
+    /// Client for requesting a VAT number validation from the VIES service.
+    /// </summary>
+    public sealed class ViesClient : IDisposable
+    {
+        private readonly HttpClient _client;
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public ViesClient(TimeSpan? timeout = null)
+        {
+            _client = timeout == null ? SetupClient(null) : SetupClient(timeout);
+        }
 
         /// <summary>
         /// Validate a VAT number for a specific country.
@@ -30,11 +29,41 @@ namespace CoderBash.Net.Vies.Clients
         /// <param name="cancellationToken"></param>
         /// <returns><see cref="VatValidationResponse"/> object.</returns>
         public async Task<VatValidationResponse> ValidateVatNumberAsync(string countryCode, string vatNumber, CancellationToken cancellationToken = default)
-		{
-			var country = (EUCountryCodes)Enum.Parse(typeof(EUCountryCodes), countryCode);
+        {
+            var country = (EUCountryCodes)Enum.Parse(typeof(EUCountryCodes), countryCode);
 
-			return await ValidateVatNumberAsync(country, vatNumber, cancellationToken);
-		}
+            return await ValidateVatNumberAsync(country, vatNumber, cancellationToken);
+        }
+
+        /// <summary>
+        /// Validates multiple VAT numbers
+        /// </summary>
+        /// <param name="vatNumbers">List of VAT numbers</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public async Task<IList<VatValidationResponse>> ValidateMultipleVatNumbersAsync(IEnumerable<string> vatNumbers, CancellationToken cancellationToken = default)
+        {
+            var result = new List<VatValidationResponse>();
+            var tasks = new List<Task>();
+
+            foreach (var vn in vatNumbers)
+            {
+                await Task.Delay(400).ContinueWith(async _ =>
+                {
+                    try
+                    {
+                        result.Add(await ValidateVatNumberAsync(vn.Substring(0, 2), vn.Substring(2), cancellationToken));
+                    }
+                    catch (Exception ex)
+                    {
+                        result.Add(new VatValidationResponse() { IsValid = false, VatNumber = vn });
+                    }
+                });
+            };
+            _ = Task.WhenAll(tasks);
+            return result;
+        }
+
 
         /// <summary>
         /// Validate a VAT number for a specific country.
@@ -46,66 +75,68 @@ namespace CoderBash.Net.Vies.Clients
         /// <exception cref="ViesRequestException"></exception>
         /// <exception cref="ViesModelException"></exception>
         public async Task<VatValidationResponse> ValidateVatNumberAsync(EUCountryCodes country, string vatNumber, CancellationToken cancellationToken = default)
-		{
-			vatNumber = vatNumber.Replace(" ", "")
-				.Replace(".", "")
-				.Replace(country.ToString(), "")
-				.Trim();
+        {
+            vatNumber = vatNumber.Replace(" ", "")
+                .Replace(".", "")
+                .Replace(country.ToString(), "")
+                .Trim();
 
-			var response = await _client.GetAsync($"{country}/vat/{vatNumber}", cancellationToken);
+            var response = await _client.GetAsync($"{country}/vat/{vatNumber}", cancellationToken);
 
-			if (!response.IsSuccessStatusCode)
-			{
-				throw new ViesRequestException($"An error occured fetching VAT information from VIES: {response.ReasonPhrase}");
-			}
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ViesRequestException($"An error occured fetching VAT information from VIES: {response.ReasonPhrase}");
+            }
 
-			var responseContent = await response.Content.ReadAsStringAsync(cancellationToken);
+            var responseContent = await response.Content.ReadAsStringAsync(cancellationToken);
 
-			if (responseContent == null)
-			{
-				throw new ViesRequestException("Unable to read response content from VAT information request.");
-			}
+            if (responseContent == null)
+            {
+                throw new ViesRequestException("Unable to read response content from VAT information request.");
+            }
 
-			var viesResponse = JsonConvert.DeserializeObject<ViesResponse>(responseContent);
+            var viesResponse = JsonConvert.DeserializeObject<ViesResponse>(responseContent);
 
-			if (viesResponse == null)
-			{
-				throw new ViesModelException("Could not deserialize expected VIES model from response content of the VAT information request.");
-			}
+            if (viesResponse == null)
+            {
+                throw new ViesModelException("Could not deserialize expected VIES model from response content of the VAT information request.");
+            }
 
-			return new VatValidationResponse
-			{
-				Address = viesResponse.Address,
-				Country = country,
-				IsValid = viesResponse.IsValid,
-				Name = viesResponse.Name,
-				RequestDate = viesResponse.RequestDate,
-				VatNumber = viesResponse.VatNumber
-			};
-		}
+            return new VatValidationResponse
+            {
+                Address = viesResponse.Address,
+                Country = country,
+                IsValid = viesResponse.IsValid,
+                Name = viesResponse.Name,
+                RequestDate = viesResponse.RequestDate,
+                VatNumber = viesResponse.VatNumber
+            };
+        }
 
         #region IDisposable implementation
-		/// <summary>
-		/// 
-		/// </summary>
-		public void Dispose()
-		{
-			GC.SuppressFinalize(this);
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
 
-			_client.Dispose();
-		}
+            _client.Dispose();
+        }
         #endregion
 
         #region Setup
-		private static HttpClient SetupClient()
-		{
-			var client = new HttpClient()
-			{
-				BaseAddress = new Uri("https://ec.europa.eu/taxation_customs/vies/rest-api/ms/")
-			};
+        private static HttpClient SetupClient(TimeSpan? timeout)
+        {
+            var client = new HttpClient()
+            {
+                BaseAddress = new Uri("https://ec.europa.eu/taxation_customs/vies/rest-api/ms/"),
+                Timeout = timeout ?? new TimeSpan(0, 1, 0)
+            };
 
-			return client;
-		}
+            return client;
+        }
+
         #endregion
     }
 }
